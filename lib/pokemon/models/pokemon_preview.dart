2ef9c35aaa254c84125dfc3277b34bbbd9ff631f
ivan nagara <ivannagara@gmail.com>
2023-04-10 00:21:20 +0800

    feat: pokemon preview model

--- lib/pokemon/models/pokemon_preview.dart
+++ lib/pokemon/models/pokemon_preview.dart
@@ -1,9 +1,27 @@
-class PokemonPreview {
+import 'package:equatable/equatable.dart';
+
+class PokemonPreview extends Equatable {
   const PokemonPreview({
     this.name = '',
     this.url = '',
   });
-  
+
+  factory PokemonPreview.fromJson(Map<String, dynamic> json) {
+    final name = json['name'] as String?;
+    final url = json['url'] as String?;
+
+    return PokemonPreview(
+      name: name ?? '',
+      url: url ?? '',
+    );
+  }
+
   final String name;
   final String url;
+
+  @override
+  List<Object?> get props => [
+        name,
+        url,
+      ];
 }
